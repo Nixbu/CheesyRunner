cc9c630ecf000949b2d72d01b9376922b05ef1c3
Nixbu <83171543+Nixbu@users.noreply.github.com>
2024-02-11 16:12:38 +0200

    # Added getSprite()

--- include/GameObject.h
+++ include/GameObject.h
@@ -7,8 +7,12 @@ class GameObject {
 public:
 	GameObject(sf::Vector2f position, sf::Texture * texture);
 	virtual ~GameObject() = default;
+	
+	const sf::Sprite* getSprite() const;
 
 	virtual void draw() = 0;
+
+	
 private:
 	sf::Sprite m_sprite;
 };
