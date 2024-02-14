378e521b29bc3359d9f65476b769896ed8a7a637
Nixbu <83171543+Nixbu@users.noreply.github.com>
2024-02-14 11:01:31 +0200

    Added texture_t enum

--- include/Settings.h
+++ include/Settings.h
@@ -1,3 +1,15 @@
 #pragma once
 
-enum Gift_t = { freeze , killCat , addTime  };
+enum Gift_t { freeze , killCat , addTime  };
+
+enum Texture_t {mouseTexture, 
+				catTexture,
+				wallTexture,
+				cheeseTexture,
+				giftTexture,
+				keyTexture,
+				doorTexture,
+				bgTileTexture,
+				exitBtnTexture,
+				playBtnTexture,
+				helpBtnTexture};
--- include/TextureManager.h
+++ include/TextureManager.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <vector>
+#include <SFML/Graphics.hpp>
+
 class TextureManager {
+public:
 	TextureManager();
 	~TextureManager() = default;
-
-
+private:
+	std::vector<sf::Texture> m_textures;
 };
