6c38c96536e87f2161c22438de69a26f79c3d3a0
Nixbu <83171543+Nixbu@users.noreply.github.com>
2024-02-14 11:57:52 +0200

    add enum Texture_t

--- include/Settings.h
+++ include/Settings.h
@@ -1,3 +1,17 @@
 #pragma once
 
 enum Gift_t { freeze , killCat , addTime  };
+
+enum Texture_t {
+	mouseTexture,
+	catTexture,
+	wallTexture,
+	cheeseTexture,
+	giftTexture,
+	keyTexture,
+	doorTexture,
+	bgTileTexture,
+	exitBtnTexture,
+	playBtnTexture,
+	helpBtnTexture
+};
