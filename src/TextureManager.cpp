c4e37aabd628813d27f298b0ee2e7f8f00707c43
Nixbu <83171543+Nixbu@users.noreply.github.com>
2024-02-14 11:52:51 +0200

    Added texture manager, removed old files

--- src/TextureManager.cpp
+++ src/TextureManager.cpp
@@ -1,5 +1,60 @@
 #include "TextureManager.h"
 
+//====================================================================
+// Constructor
 TextureManager::TextureManager()
-{
+{   
+    sf::Texture currentTexture;
+
+    load(currentTexture, "mouse.png");
+    m_textures.push_back(currentTexture);
+
+    load(currentTexture, "cat.png");
+    m_textures.push_back(currentTexture);
+
+    load(currentTexture, "wall.png");
+    m_textures.push_back(currentTexture);
+
+    load(currentTexture, "cheese.png");
+    m_textures.push_back(currentTexture);
+
+    load(currentTexture, "gift.png");
+    m_textures.push_back(currentTexture);
+
+    load(currentTexture, "key.png");
+    m_textures.push_back(currentTexture);
+
+    load(currentTexture, "door.png");
+    m_textures.push_back(currentTexture);
+
+    load(currentTexture, "playButton.png");
+    m_textures.push_back(currentTexture);
+
+    load(currentTexture, "exitButton.png");
+    m_textures.push_back(currentTexture);
+
+    load(currentTexture, "helpButton.png");
+    m_textures.push_back(currentTexture);
+
 }
+
+//===================================================
+// Get pointer to wanted texture
+sf::Texture* TextureManager::getTexture(Texture_t wanted_texture) const
+{
+    return (sf::Texture*)&m_textures[wanted_texture];
+}
+
+
+//===================================================
+// Load texture from file
+void TextureManager::load(sf::Texture& current, 
+    std::string texture_name)
+{
+    if (!current.loadFromFile(texture_name))
+    {
+        std::cout << "faild to load texture " << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
