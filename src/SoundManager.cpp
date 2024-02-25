a155007dd5dac3159f94b5752c689cfa0b045965
Nixbu <83171543+Nixbu@users.noreply.github.com>
2024-02-25 02:30:32 +0200

    end of Saturday

--- src/SoundManager.cpp
+++ src/SoundManager.cpp
@@ -6,12 +6,9 @@ SoundManager::SoundManager()
 
 	load(currentSndBffr, "keySound.ogg");
 	m_sounds.push_back(currentSndBffr);
-
-    sf::Sound sound;
-
-    sound.setBuffer(m_sounds[keySound]);
-    sound.setVolume(100);
-    sound.play();
+    // WORKING - Just need to do it in a function, and for more sounds
+    m_keySound.setBuffer(m_sounds[keySound]);
+    m_keySound.setVolume(SFX_VOLUME);
 }
 
 sf::SoundBuffer* SoundManager::getSoundBuffer(Sound_t sound) const
@@ -21,10 +18,7 @@ sf::SoundBuffer* SoundManager::getSoundBuffer(Sound_t sound) const
 
 void SoundManager::playKeySound()
 {
-    m_keySound.setBuffer(m_sounds[keySound]);
     m_keySound.play();
-    std::cout << "Got key\n";
-
 }
 
 void SoundManager::load(sf::SoundBuffer & current, std::string soundName)
