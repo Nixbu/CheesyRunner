d147558f5cae20560d32480b32aa6e4f75c23008
itay mijan <itaym99@gmail.com>
2024-02-28 17:41:21 +0200

    until level

--- src/Key.cpp
+++ src/Key.cpp
@@ -4,29 +4,28 @@ void Key::handleCollision(GameObject& gameObject, sf::FloatRect intersection)
 {
 	gameObject.handleCollision(*this, intersection);
 }
-
+//======================================================
 void Key::handleCollision(Mouse& gameObject, sf::FloatRect intersection)
 {
 	gameObject.handleCollision(*this, intersection);
 }
-
-
+//======================================================
 void Key::handleCollision(Cheese& gameObject, sf::FloatRect intersection)
 {
 }
-
+//======================================================
 void Key::handleCollision(Door& gameObject, sf::FloatRect intersection)
 {
 }
-
+//======================================================
 void Key::handleCollision(Cat& gameObject, sf::FloatRect intersection)
 {
 }
-
+//======================================================
 void Key::handleCollision(Key& gameObject, sf::FloatRect intersection)
 {
 }
-
+//======================================================
 void Key::handleCollision(Wall& gameObject, sf::FloatRect intersection)
 {
 }
